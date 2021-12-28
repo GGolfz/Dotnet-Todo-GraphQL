9f02804b6e76ab306c75ac3e5b50ea815d39d288
GGolfz <wisarutgolf11@gmail.com>
2021-12-28 23:30:22 +0700

    add: edit & delete todo item :sparkles:

--- Models/Mutation.cs
+++ Models/Mutation.cs
@@ -1,4 +1,5 @@
 using HotChocolate;
+using Microsoft.AspNetCore.Mvc;
 namespace TodoGraphQL.Models
 {
     public class Mutation {
@@ -12,6 +13,22 @@ namespace TodoGraphQL.Models
             context.TodoItems.Add(item);
             await context.SaveChangesAsync();
             return item;
+        }
+        public async Task<TodoItem?> EditTodoItem(long id, string name, string description, bool isComplete, [Service] TodoContext context) {
+            var item = await context.TodoItems.FindAsync(id);
+            if (item == null) return null;
+            item.Name = name;
+            item.Description = description;
+            item.IsComplete = isComplete;
+            await context.SaveChangesAsync();
+            return item;
+        } 
+        public async Task<bool?> DeleteTodoItem(long id, [Service] TodoContext context) {
+            var item = await context.TodoItems.FindAsync(id);
+            if(item == null) return null;
+            context.TodoItems.Remove(item);
+            await context.SaveChangesAsync();
+            return true;
         }
     }
 }
